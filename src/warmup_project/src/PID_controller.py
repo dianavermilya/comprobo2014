424a546512d6936bbcde158771ef567e9cfa83c3
Diana Vermilya <dianavermilya@gmail.com>
2014-09-21 22:11:09 -0400

    wall follower and object follower

--- src/warmup_project/src/PID_controller.py
+++ src/warmup_project/src/PID_controller.py
@@ -1,14 +1,23 @@
+import math
+
 class PIDcontroller(object):
     def __init__(self, name="rbt"):
         self.name = name
-        self.distanceFromWall = None
-        self.xVel = 0
+        self._distanceFromWall = 0
+        self._angleOfWallNormal = 0
+        self.rotation = 0
     def setDistanceFromWall(self, distance):
-    	self.distanceFromWall = distance
-    def getDistanceFromWall(self):
-        return self.distanceFromWall
-    def setXVel(self):
-        self.xVel = 0.1*(self.distanceFromWall - 0.2)
-    def getXVel(self):
-    	return self.xVel
+    	self._distanceFromWall = distance
+    def setAngleOfWallNormal(self, smallest_idx):
+        self._angleOfWallNormal = smallest_idx
+
+    def calculateRotationToFollowWall(self):
+        beNormalToWall = (self._angleOfWallNormal-90)*(0.01)
+        beNearWall = (self._distanceFromWall - 0.3)*(0.8)
+        print self._angleOfWallNormal, self._distanceFromWall
+        print beNormalToWall, beNearWall
+        self.rotation = beNormalToWall + beNearWall        
+
+    def calculateRotationToFollowObject(self, error):
+        self.rotation = error*0.01
 
--- src/warmup_project/src/wallFollower.py
+++ src/warmup_project/src/wallFollower.py
@@ -2,43 +2,86 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from PID_controller import PIDcontroller
+import math
 
 
 class WallFollower(object):
     def __init__(self):
         self.pid = PIDcontroller("Ribbit")
-        self.distanceFromWall = None
-        self.xVel = 0
 
 
     def runController(self):
+        print "run Controller"
         count = 0
         pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=10)
-        rospy.Subscriber("scan", LaserScan, self.callback)
+        rospy.Subscriber("scan", LaserScan, self.detect)
         rospy.init_node('controller', anonymous=True)
         r = rospy.Rate(10) #hz
         message = Twist()
         while not rospy.is_shutdown():
-            count = self.detectWall(count)
-            message.linear.x = self.pid.getXVel()
+            rotation = self.pid.rotation
+            message.angular.z = rotation
+            message.linear.x = 0.1
             pub.publish(message)
+    
+    def detect(self, data):
+        arr = data.ranges
+        avgs = []
+
+        for i in range(360):
+            count = 0
+            total = 0
+            for j in range(-2, 3):
+                val = arr[(i+j)%360]
+                if val != 0.0:
+                    total += val
+                    count += 1
+            if count == 0:
+                avg = 1000000.0
+            else:
+                avg = total/count
+            avgs.append(avg)
+        if not self.detectObject(avgs):
+            self.detectWall(avgs)
+    
+    def detectObject(self, avgs):
+        first = None
+        angle_seg = None
+        for i in range(len(avgs)):
+            if avgs[i] < 0.5 and first == None:
+                    first = i
+            elif avgs[i] > 0.5 and first != None:
+                angle_seg = [first, i]
+                first = None
+
+        if angle_seg == None:
+            return False
+            
+        if angle_seg[0] == 0 and first != None:
+            angle_seg[0] = first - 360
+     
+        if angle_seg[1] - angle_seg[0] < 45:
+            index_range = (angle_seg[1] - angle_seg[0] + 360) % 360
+            central_index = (int(math.floor(index_range/2.0)) + angle_seg[0]) % 360
+            if central_index > 180:
+                error = 360 - central_index
+            else:
+                error = central_index
+            self.pid.calculateRotationToFollowObject(error)
+            print "follow object", error
+            return True
+        return False
 
 
-    def callback(self, data):
-        r = data.ranges
-        avg =(r[358] + r[359] + r[0] + r[1] + r[2])/5
-        self.pid.setDistanceFromWall(avg)
-        self.pid.setXVel()
-        print avg
 
+    def detectWall(self, avgs):
+        smallest = min(avgs)
+        smallest_idx = avgs.index(smallest)
+        self.pid.setDistanceFromWall(smallest)
+        self.pid.setAngleOfWallNormal(smallest_idx)
+        self.pid.calculateRotationToFollowWall()
         #rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
-        self.pid.setDistanceFromWall(2.10)
-        print len(data.ranges)
 
-    def detectWall(self, count):
-        #rospy.init_node('listener', anonymous=True)
-        count = count + 1
-        return count
     
 
 
